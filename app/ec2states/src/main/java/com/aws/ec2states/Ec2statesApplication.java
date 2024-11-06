370f7d6ab7e61ad619373bffc6b132b379ca2e20
Daniel Carrasco Baez <daniel.carrasco.baez@accenture.com>
2024-11-06 11:59:26 -0300

    refactoring del proyecto

---
+++ app/ec2states/src/main/java/com/aws/ec2states/Ec2statesApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ec2states;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2statesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2statesApplication.class, args);
+	}
+
+}
---
+++ app/ec2states/src/test/java/com/aws/ec2states/Ec2statesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.ec2states;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2statesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
